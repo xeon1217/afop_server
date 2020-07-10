46f88cc8066fae82d59496b13eb1d448f0e09a69
jungyoon <xeon1217@gmail.com>
2020-07-11 03:37:08 +0900

    서버 동작 로직 변경

--- src/main/kotlin/com/example/afop_server/AfopServerApplication.kt
+++ src/main/kotlin/com/example/afop_server/AfopServerApplication.kt
@@ -1,13 +1,29 @@
 package com.example.afop_server
 
+import com.example.afop_server.Config.GracefulShutdown
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @SpringBootApplication
-class AfopServerApplication
+class AfopServerApplication {
+    @Bean
+    fun gracefulShutdown(): GracefulShutdown {
+        return GracefulShutdown()
+    }
+
+    @Bean
+    fun webServerFactory(gracefulShutdown: GracefulShutdown): ConfigurableServletWebServerFactory {
+        val factory: TomcatServletWebServerFactory = TomcatServletWebServerFactory()
+        factory.addConnectorCustomizers(gracefulShutdown)
+        return factory
+    }
+}
+
 fun main(args: Array<String>) {
     runApplication<AfopServerApplication>(*args)
 }
---
+++ src/main/kotlin/com/example/afop_server/Config/GracefulShutdown.kt
@@ -0,0 +1,47 @@
+package com.example.afop_server.Config
+
+import com.example.afop_server.Common.Log
+import org.apache.catalina.connector.Connector
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer
+import org.springframework.context.ApplicationListener
+import org.springframework.context.event.ContextClosedEvent
+import java.util.concurrent.Executor
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+
+class GracefulShutdown : TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+    private val tag = GracefulShutdown::class.simpleName
+    private val TIMEOUT: Long = 60 // 강제종료까지 걸리는 시간
+    @Volatile
+    private var connector: Connector? = null
+
+    override fun customize(connector: Connector?) {
+        this.connector = connector
+    }
+
+    override fun onApplicationEvent(event: ContextClosedEvent) {
+        connector?.let {
+            it.pause()
+            val executor = it.protocolHandler.executor
+
+            if (executor is ThreadPoolExecutor) {
+                try {
+                    val threadPoolExecutor: ThreadPoolExecutor = executor as ThreadPoolExecutor
+                    threadPoolExecutor.shutdown()
+                    if(!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+                        Log.warn(tag, "Tomcat thread pool did not shut down gracefully within $TIMEOUT seconds. Proceeding with forceful shutdown")
+                        threadPoolExecutor.shutdownNow()
+
+                        if(!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+                            Log.error(tag, "Tomcat thread pool did not terminate")
+                        }
+                    } else {
+                        Log.info(tag, "Tomcat thread pool has been gracefully shutdown")
+                    }
+                } catch (e: InterruptedException) {
+                    Thread.currentThread().interrupt()
+                }
+            }
+        }
+    }
+}
