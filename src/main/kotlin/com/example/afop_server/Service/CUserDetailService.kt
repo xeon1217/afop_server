0d032aed0b9ce5225ff530ca1586f97ff059e00e
xeon1 <xeon1217@gmail.com>
2020-11-13 08:53:26 +0900

    보안 관련 수정

--- src/main/kotlin/com/example/afop_server/Service/CUserDetailService.kt
+++ src/main/kotlin/com/example/afop_server/Service/CUserDetailService.kt
@@ -3,13 +3,12 @@ package com.example.afop_server.Service
 import com.example.afop_server.Advice.Exception.Auth.UserNotFoundException
 import com.example.afop_server.Model.User
 import com.example.afop_server.Repository.UserRepository
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
 class CUserDetailService(private val userRepository: UserRepository) : UserDetailsService {
-    override fun loadUserByUsername(username: String): User {
+    override fun loadUserByUsername(username: String): User? {
         return userRepository.findById(username.toLong()).orElseThrow(::UserNotFoundException)
     }
 }
